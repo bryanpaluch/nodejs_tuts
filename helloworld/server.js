f180fb2accd7c7a840ad13ca19cf04c77eaea50e
New Media <newmedia@newMedia-Latitude-D630.(none)>
2013-05-17 15:39:09 -0400

    aded vm

--- helloworld/server.js
+++ helloworld/server.js
@@ -1,9 +1,10 @@
 var http = require('http');
-
-http.createServer(function (req, res){
+var server = http.createServer(function (req, res){
   console.log('Server hit!, responding!');
   res.writeHead(200, {'Content-Type':'text/plain'});
   res.end('Hello World\n');
-}).listen(3000);
+});
+
+server.listen(3000);
 
 console.log('server listening on 3000');
